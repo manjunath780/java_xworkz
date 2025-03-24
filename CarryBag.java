0b68d9f4e4eca3e8bd3bae42fd057bd6adc64a02
manjunath780 <manjunath.xworkz@outlook.com>
2025-03-24 11:13:56 +0530

    Update CarryBag.java

--- CarryBag.java
+++ CarryBag.java
@@ -1,23 +1,22 @@
 class CarryBag {
     String material;
-    String size; // Size (e.g., small, medium, large)
-    double price; // Price in USD
+    String size;
+    double price; 
 
-    // No-argument constructor (using reference variable)
+    
     CarryBag() {
         material = "Plastic";
         size = "Medium";
-        price = 2.0; // Default price in USD
+        price = 2.0; 
     }
 
-    // Parameterized constructor (initialize variables inside constructor)
+    
     CarryBag(String material, String size, double price) {
         this.material = material;
         this.size = size;
         this.price = price;
     }
 
-    // To print object details
     void displayDetails() {
         System.out.println("CarryBag [Material: " + material + ", Size: " + size + ", Price: $" + price + "]");
     }
