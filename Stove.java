428ca8d611ca533d36974e3ded8dd9965735f5ed
manjunath780 <manjunath.xworkz@outlook.com>
2025-03-24 11:27:45 +0530

    Update Stove.java

--- Stove.java
+++ Stove.java
@@ -5,21 +5,21 @@ class Stove {
     int burnerCount;
     String fuelType;
 
-    // No-argument constructor (using reference variable)
+    
     Stove() {
         brand = "Samsung";
         burnerCount = 4;
         fuelType = "Gas";
     }
 
-    // Parameterized constructor (initialize variables inside constructor)
+    
     Stove(String brand, int burnerCount, String fuelType) {
         this.brand = brand;
         this.burnerCount = burnerCount;
         this.fuelType = fuelType;
     }
 
-    // To print object details
+    
     void displayDetails() {
         System.out.println("Stove [Brand: " + brand + ", Burner Count: " + burnerCount + ", Fuel Type: " + fuelType + "]");
     }
