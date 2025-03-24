56e79f669e083e812553507d8d680844185b0c6d
manjunath780 <manjunath.xworkz@outlook.com>
2025-03-24 11:25:05 +0530

    Update Cosmetic.java

--- Cosmetic.java
+++ Cosmetic.java
@@ -1,23 +1,23 @@
 class Cosmetic {
     String brand;
     String productType;
-    double price; // Price in USD
+    double price; 
 
-    // No-argument constructor (using reference variable)
+    
     Cosmetic() {
         brand = "Maybelline";
         productType = "Lipstick";
-        price = 15.0; // Default price in USD
+        price = 15.0; 
     }
 
-    // Parameterized constructor (initialize variables inside constructor)
+    
     Cosmetic(String brand, String productType, double price) {
         this.brand = brand;
         this.productType = productType;
         this.price = price;
     }
 
-    // To print object details
+
     void displayDetails() {
         System.out.println("Cosmetic [Brand: " + brand + ", Product Type: " + productType + ", Price: $" + price + "]");
     }
