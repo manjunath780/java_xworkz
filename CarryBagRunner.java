1bc4efaf5a7aad8b635abf424eda0d6c2f7d7320
manjunath780 <manjunath.xworkz@outlook.com>
2025-03-24 11:20:32 +0530

    Update CarryBagRunner.java

--- CarryBagRunner.java
+++ CarryBagRunner.java
@@ -1,24 +1,24 @@
 public class CarryBagRunner {
     public static void main(String[] args) {
-        // Creating instances using no-argument constructor
-        CarryBag carryBag1 = new CarryBag(); // Default values
-        CarryBag carryBag2 = new CarryBag(); // Default values
+        
+        CarryBag carryBag1 = new CarryBag(); 
+        CarryBag carryBag2 = new CarryBag(); 
         carryBag2.material = "Jute";
         carryBag2.size = "Large";
-        carryBag2.price = 5.0; // Custom values
+        carryBag2.price = 5.0; 
 
-        CarryBag carryBag3 = new CarryBag(); // Default values
+        CarryBag carryBag3 = new CarryBag(); 
         carryBag3.material = "Cotton";
         carryBag3.size = "Medium";
-        carryBag3.price = 3.0; // Custom values
+        carryBag3.price = 3.0; 
 
-        // Creating instances using parameterized constructor
-        CarryBag carryBag4 = new CarryBag("Plastic", "Small", 2.0); // Custom values
-        CarryBag carryBag5 = new CarryBag("Canvas", "Large", 6.0); // Custom values
-        CarryBag carryBag6 = new CarryBag("Paper", "Medium", 4.0); // Custom values
-        CarryBag carryBag7 = new CarryBag("Recycled Material", "Large", 7.0); // Custom values
+        
+        CarryBag carryBag4 = new CarryBag("Plastic", "Small", 2.0); 
+        CarryBag carryBag5 = new CarryBag("Canvas", "Large", 6.0); 
+        CarryBag carryBag6 = new CarryBag("Paper", "Medium", 4.0);
+        CarryBag carryBag7 = new CarryBag("Recycled Material", "Large", 7.0); 
 
-        // Displaying details of all instances
+        
         System.out.println("CarryBag Instances:");
         carryBag1.displayDetails();
         carryBag2.displayDetails();
