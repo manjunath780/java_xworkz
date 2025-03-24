56cc3da7fd6f89670d9962d80925a3d774c2ea9d
manjunath780 <manjunath.xworkz@outlook.com>
2025-03-24 11:21:43 +0530

    Update Chain.java

--- Chain.java
+++ Chain.java
@@ -1,23 +1,23 @@
 class Chain {
     String material;
     String type;
-    double length; // Length in centimeters
+    double length; 
 
-    // No-argument constructor
+    
     Chain() {
         material = "Gold";
         type = "Necklace";
-        length = 45.0; // Default length in centimeters
+        length = 45.0; 
     }
 
-    // Parameterized constructor
+    
     Chain(String material, String type, double length) {
         this.material = material;
         this.type = type;
         this.length = length;
     }
 
-    // To print object details
+    
     void displayDetails() {
         System.out.println("Chain [Material: " + material + ", Type: " + type + ", Length: " + length + " cm]");
     }
