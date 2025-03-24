5ad76569e393238d0fec289f3745a2eba427370d
manjunath780 <manjunath.xworkz@outlook.com>
2025-03-24 10:59:37 +0530

    Update Bell.java

--- Bell.java
+++ Bell.java
@@ -1,23 +1,23 @@
 class Bell {
     String material;
     String color;
-    String sound; // Sound description
+    String sound; 
 
-    // No-argument constructor
+    
     Bell() {
         material = "Brass";
         color = "Gold";
-        sound = "Ding"; // Default sound
+        sound = "Ding"; 
     }
 
-    // Parameterized constructor
+    
     Bell(String material, String color, String sound) {
         this.material = material;
         this.color = color;
         this.sound = sound;
     }
 
-    // To print object details
+    
     void displayDetails() {
         System.out.println("Bell [Material: " + material + ", Color: " + color + ", Sound: " + sound + "]");
     }
